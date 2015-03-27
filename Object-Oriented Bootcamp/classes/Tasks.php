55ecdad5b47312989b11c7b48427086b595ef31c
RamaneekGill <ramaneek.gill@hotmail.com>
2015-03-27 18:48:40 -0400

    Finished the classes tutorial

--- Object-Oriented Bootcamp/classes/Tasks.php
+++ Object-Oriented Bootcamp/classes/Tasks.php
@@ -1,13 +1,26 @@
 <?php
 
 class Task {
-	//public means outside of this class anyone can access this property 
-	public $description = 'Go to the store';
+	//public means outside of this class anyone can access this instance 
+	public $description;
+	public $completed = false;
 
+	//this function is called everytime a new class is created
+	public function __construct($description) {
+		//$this refers to this object
+		//more specifically this instance of this class
+		$this->description = $description;
+	}
+
+	public function complete() {
+		$this->completed = true;
+	}
 }
 
-$task = new Task();
-//to access
-var_dump($task->description)
+$task = new Task('learn OOP');
+$task->complete();
+$task2 = new Task('groceries');
+var_dump($task->completed);
+var_dump($task2->description);
 
 ?>
