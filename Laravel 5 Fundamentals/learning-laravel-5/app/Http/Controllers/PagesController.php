871ee2006db97d51c39f94af9a0240222648ead9
RamaneekGill <ramaneek.gill@hotmail.com>
2015-04-01 23:23:29 -0400

    learned how to pass data into views

---
+++ Laravel 5 Fundamentals/learning-laravel-5/app/Http/Controllers/PagesController.php
@@ -0,0 +1,34 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller {
+
+	public function about() {
+		//return 'About Page';
+		//$name = 'Ramaneek <span style="color: red;">Gill</span>';
+		//two ways of passing $name into the view
+		//return view('pages.about')->with('name', $name);
+		//to pass more than one variable use an array!
+		return view('pages.about')->with([
+				'first' => 'Ramaneek',
+				'last' => 'Gill'
+			]);
+
+		//third way:
+		//pass data as an arr in the 
+		//second arg in the view() function
+
+		//how to make arr in PHP quickly:
+		/*
+		$var1 = 'mario';
+		$var2 = 'luigi';
+		$arr = compact('var1', 'var2');
+		return view('pages.about', $arr);
+		*/
+	}
+
+}
---
+++ Laravel 5 Fundamentals/learning-laravel-5/resources/views/pages/about.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang='en'>
+<head>
+	<meta charset="utf-8">
+	<title>Document</title>
+</head>
+
+<body>
+	<h1>About Me!</h1>
+
+	<p>{{$first}} {{$last}}</p>
+
+</body>
+
+</html>
