ee31e8bab95cd6ac961e4365180a01ccd350d73d
RamaneekGill <ramaneek.gill@hotmail.com>
2015-04-18 23:45:42 -0400

    finished many to many relations lesson

--- Laravel 5 Fundamentals/learning-laravel-5/app/Article.php
+++ Laravel 5 Fundamentals/learning-laravel-5/app/Article.php
@@ -34,4 +34,8 @@ class Article extends Model {
 	public function user() {
 		return $this->belongsTo('App\User');
 	}
+
+	public function tags(){
+		return $this->belongsToMany('App\Tag')->withTimestamps();
+	}
 }
---
+++ Laravel 5 Fundamentals/learning-laravel-5/app/Tag.php
@@ -0,0 +1,10 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model {
+
+	public function articles() {
+		return $this->belongsToMany('App\Article');
+	}
+} 
